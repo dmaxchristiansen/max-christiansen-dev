c00c505f141b0a96fd404ab6fbe61b26a3ee1875
Daniel Christiansen <dmaxchristiansen@gmail.com>
2022-12-14 20:15:16 -0700

    MCD-11 Init commit for async form submission

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
 import { useInView, defaultFallbackInView } from "react-intersection-observer";
 import styled from "styled-components";
 import Layout from "src/components/global/Layout/Layout";
 import Seo from "src/components/global/Seo/Seo";
 import SectionHeader from "src/components/global/SectionHeader/SectionHeader";
-import { BLACK, OBSIDIAN, ROYAL_BLUE, WHITE } from "src/styles/colors";
+import { BLACK, OBSIDIAN, ROYAL_BLUE, BLUE_EYES } from "src/styles/colors";
+import { ACCORDION_BOX_SHADOW } from "src/components/homepage/Experience/Accordion/utils/constants";
+import { FAST } from "src/utils/constants/transition-speeds";
 
 const Container = styled.div`
   max-width: 1350px;
@@ -41,10 +44,16 @@ const Input = styled.input`
   height: auto;
   width: 100%;
   padding: 16px;
-  border: 2px solid ${WHITE};
+  border: none;
   border-radius: 6px;
+  box-shadow: ${ACCORDION_BOX_SHADOW};
+  outline: none;
   color: ${BLACK};
   font-size: 18px;
+  transition: box-shadow ${FAST};
+  &:focus {
+    box-shadow: 0 0 8px 6px ${BLUE_EYES};
+  }
 `;
 
 const TextAreaWrapper = styled.div`
@@ -56,26 +65,81 @@ const TextArea = styled.textarea`
   height: auto;
   width: 100%;
   padding: 16px;
-  border: 2px solid ${WHITE};
+  border: none;
   border-radius: 6px;
+  box-shadow: ${ACCORDION_BOX_SHADOW};
+  outline: none;
   color: ${BLACK};
   font-size: 18px;
   resize: vertical;
+  transition: box-shadow ${FAST};
+  &:focus {
+    box-shadow: 0 0 8px 6px ${BLUE_EYES};
+  }
 `;
 
 const Button = styled.button`
   display: block;
   margin: 0 auto;
   padding: 8px 16px;
-  background-color: ${OBSIDIAN};
-  border: 2px solid ${ROYAL_BLUE};
+  background-color: ${ROYAL_BLUE};
+  border: none;
   border-radius: 6px;
+  box-shadow: ${ACCORDION_BOX_SHADOW};
+  outline: none;
   font-weight: 900;
   font-size: 18px;
   letter-spacing: 4px;
+  cursor: pointer;
+  transition: box-shadow ${FAST}, background-color ${FAST};
+  &:hover {
+    background-color: ${OBSIDIAN};
+    box-shadow: 0 0 8px 6px ${BLUE_EYES};
+  }
+  &:focus {
+    background-color: ${OBSIDIAN};
+    box-shadow: 0 0 8px 6px ${BLUE_EYES};
+  }
 `;
 
+const encode = (data: Record<string, string>) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 const ContactPage = () => {
+  const [formState, setFormState] = useState({});
+  const [isThankYouContentVisible, setIsThankYouContentVisible] =
+    useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": "contact",
+        ...formState,
+      }),
+    })
+      .then(() => {
+        const submitButton = document.getElementById("submitButton");
+        if (submitButton) {
+          submitButton.setAttribute("disabled", "true");
+        }
+      })
+      .then(() => setIsThankYouContentVisible(true))
+      .catch(error => alert(error));
+  };
+
   defaultFallbackInView(true);
 
   const { ref: expertiseRef, inView: isExpertiseVisible } = useInView({
@@ -94,29 +158,49 @@ const ContactPage = () => {
             delayTransition
           />
         </HeaderWrapper>
+        {isThankYouContentVisible && (
+          <div style={{ fontSize: 100 }}>Oh hell yes yeah</div>
+        )}
         <Form
           name="contact"
           method="POST"
           data-netlify="true"
           data-netlify-honeypot="true"
+          onSubmit={e => void handleSubmit(e)}
         >
           <input type="hidden" name="form-name" value="contact" />
           <input type="hidden" name="bot-field" />
           <FormRow>
             <InputWrapper>
               <InputLabel>Name</InputLabel>
-              <Input type="text" name="userName" required />
+              <Input
+                type="text"
+                name="userName"
+                required
+                onChange={e => handleChange(e)}
+              />
             </InputWrapper>
             <InputWrapper>
               <InputLabel>Email</InputLabel>
-              <Input type="text" name="userEmail" required />
+              <Input
+                type="email"
+                name="userEmail"
+                required
+                onChange={e => handleChange(e)}
+              />
             </InputWrapper>
           </FormRow>
           <TextAreaWrapper>
             <InputLabel>Message</InputLabel>
-            <TextArea name="userMessage" rows={4} />
+            <TextArea
+              name="userMessage"
+              rows={4}
+              onChange={e => handleChange(e)}
+            />
           </TextAreaWrapper>
-          <Button type="submit">SUBMIT</Button>
+          <Button id="submitButton" type="submit">
+            SUBMIT
+          </Button>
         </Form>
       </Container>
     </Layout>
