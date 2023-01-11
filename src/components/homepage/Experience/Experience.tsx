96c9eca5b0b9edd03b67f8d1b38cde23044e5c70
Daniel Christiansen <dmaxchristiansen@gmail.com>
2023-01-10 17:44:01 -0700

    MCD-51 Add top margin to experience section

--- src/components/homepage/Experience/Experience.tsx
+++ src/components/homepage/Experience/Experience.tsx
@@ -10,8 +10,8 @@ import SectionHeader from "src/components/global/SectionHeader/SectionHeader";
 import Accordion from "src/components/homepage/Experience/Accordion/Accordion";
 
 const Container = styled.div`
-  padding: 0 30px;
-  margin-top: 100px;
+  padding: 50px 30px 0;
+  margin-top: 50px;
   @media (max-width: 520px) {
     padding-top: 30px;
   }
